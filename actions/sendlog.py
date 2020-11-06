a78e9b07461444666d6375103e74b554130600f3
Marcelo Arakaki <marcelo.arakaki@gmail.com>
2020-11-05 21:25:53 -0300

    Update sendlog.py

--- actions/sendlog.py
+++ actions/sendlog.py
@@ -5,14 +5,8 @@ from st2common.runners.base_action import Action
 
 class SendLogAction(Action):
     def run(self, splunk_url, index, token, event):
-        timestamp = datetime.now().strftime("%d/%m/%Y %H:%M:%S")
-        event_headers = {'Authorization': 'Splunk ' + token}
-        event_payload = json.dumps({'sourcetype':'_json', 
-                                    'index':index, 
-                                    'timestamp':timestamp, 
-                                    'source':'teste_splunk',
-                                    'host':'localhost', 
-                                    'event': {'message': event}})
+        event_headers = {'Authorization': 'Splunk ' + token, 'Content-Type': 'application/json'}
+        event_payload = json.dumps({"sourcetype": "json", "index": index, "event": event})
 
         r = requests.post(
             url = splunk_url, 
